f51855918f8a0d86a275147284d27113207ac111
Florian Spier <Florian.Spier@portofantwerp.com>
2020-11-06 03:15:42 -1200

    Date Local Value Accessor - cleanup

--- src/app/datepicker-overview-example.ts
+++ src/app/datepicker-overview-example.ts
@@ -7,25 +7,4 @@ import {Component} from '@angular/core';
 })
 export class DatepickerOverviewExample {
   date = new Date();
-
-  dateAsString = formatDate(this.date);
 }
-
-//https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd?page=1&tab=votes#tab-top
-function formatDate(d: Date) {
-    let month = '' + (d.getMonth() + 1);
-    let day = '' + d.getDate();
-    let year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('-');
-}
-
-
-/**  Copyright 2020 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
